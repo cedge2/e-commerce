c5ea72b173d868a1fce88acce990d3d6dd720b1f
Camber <cedge2@asu.edu>
2024-09-28 21:13:52 -0500

    Stripe integration / payment routes

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { addToCart, removeAllFromCart, updateQuantity } from "../controllers/cart.controlle.js";
+import { addToCart, removeAllFromCart, updateQuantity, getCartProducts } from "../controllers/cart.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, getCartProd);
+router.get("/", protectRoute, getCartProducts);
 router.post("/", protectRoute, addToCart);
 router.delete("/", protectRoute, removeAllFromCart);
 router.put("/:id", protectRoute, updateQuantity);
